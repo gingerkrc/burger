c9f08aab14648f8b282bc650d65048794550e12f
Ginger KRC <gingerkr@gmail.com>
2019-01-16 18:09:54 -0700

    schema issue

--- db/schema.sql
+++ db/schema.sql
@@ -1,8 +1,7 @@
 CREATE DATABASE burgers_db;
-USE burger_db;
+USE burgers_db;
 
-CREATE TABLE burgers
-(
+CREATE TABLE burgers (
 	id int NOT NULL AUTO_INCREMENT,
 	burger_name varchar(255) NOT NULL,
 	devoured BOOLEAN DEFAULT false,
